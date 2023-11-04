98a8a23118032c064e6ec3d3cf01e8f402d88cb6
Victor Immanuvel <60085537+VictorImmanuvel1@users.noreply.github.com>
2023-11-04 23:14:48 +0530

    Time: 1 ms (85.06%), Space: 39.8 MB (26.2%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -3,9 +3,11 @@ class Solution {
         if(n<1){
             return false;
         }
-        while(n%2==0){
-            n/=2;
+        else{
+            while(n%2==0){
+                n/=2;
+            }
         }
-        return(n==1);
+        return(n==1); 
     }
 }
